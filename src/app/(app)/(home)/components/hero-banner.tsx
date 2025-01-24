f3ecea25236457b99095d6b520712d57ef2388bb
gregory6z <gregoryrag@gmail.com>
2025-01-24 23:45:27 +0100

    add banner text overlay

--- src/app/(app)/(home)/components/hero-banner.tsx
+++ src/app/(app)/(home)/components/hero-banner.tsx
@@ -1,6 +1,7 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { BannerTextOverlay } from "./banner-text-overlay";
 
 export default function HeroBanner() {
   return (
@@ -31,16 +32,12 @@ export default function HeroBanner() {
               className="h-full w-full object-cover"
             />
           </div>
-          <div className="absolute inset-0 z-200 flex flex-col items-center justify-center rounded bg-black bg-opacity-50 p-4">
-            <h1>NOUVEAU : ÉCLAT NATUREL</h1>
-            <p>La beauté de votre peau révélée.</p>
-            <Button>DÉCOUVREZ NOS PRODUITS</Button>
-          </div>
+          <BannerTextOverlay />
         </AspectRatio>
       </div>
       <div className="hidden xl:block">
         <AspectRatio ratio={23 / 9}>
-          <div className="flex relative h-full w-full items-center justify-center bg-zinc-900">
+          <div className="relative flex h-full w-full items-center justify-center bg-zinc-900">
             <Image
               src={"/outronome.jpg"}
               width={1080}
@@ -50,19 +47,7 @@ export default function HeroBanner() {
               className="h-full w-full object-cover"
             />
           </div>
-          <div className="absolute inset-0 left-16 z-200 flex max-w-[508px] flex-col justify-center rounded bg-opacity-50 p-4">
-            <h1 className="font-bold text-5xl">NOUVEAU : ÉCLAT NATUREL</h1>
-            <p className="mt-6 text-lg">
-              <p>
-                Découvrez la beauté naturelle de votre peau avec notre gamme de
-                soins, conçue pour nourrir et revitaliser votre éclat au
-                quotidien.
-              </p>
-            </p>
-            <Button size={"xl"} className="mt-6 w-fit">
-              DÉCOUVREZ NOS PRODUITS
-            </Button>
-          </div>
+          <BannerTextOverlay />
         </AspectRatio>
       </div>
     </div>
