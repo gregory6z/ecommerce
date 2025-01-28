20f0890cef77c05831ff4a7bcdf6bb66b7ed1fc6
gregory6z <gregoryrag@gmail.com>
2025-01-28 18:47:23 +0100

    fix variants buttons

--- src/app/(app)/products/[slug]/page.tsx
+++ src/app/(app)/products/[slug]/page.tsx
@@ -24,7 +24,7 @@ export default async function Product({ params }: { params: Params }) {
     <section className="flex flex-col">
       <CarouselProduct images={product.images} />
       <div className="mt-4 grid grid-cols-3 gap-1">
-        <div className="flex items-center justify-center gap-2">
+        {/* <div className="flex items-center justify-center gap-2">
           <Button variant={"ghost"} className="text-xl">
             4.5
           </Button>
@@ -35,7 +35,7 @@ export default async function Product({ params }: { params: Params }) {
         </Button>
         <Button className="flex text-center text-xl" variant={"ghost"}>
           <Share />
-        </Button>
+        </Button> */}
       </div>
       <div className="mx-auto mt-2 flex w-full flex-col items-center px-4 ">
         <Button className="max-w-fit" size={"xs"}>
@@ -44,19 +44,28 @@ export default async function Product({ params }: { params: Params }) {
         <h1 className="mt-2 font-bold text-xl">{product.title}</h1>
         <p className="mt-2 font-bold text-2xl ">{priceFormatted}</p>
 
+        {product.variants.length > 1 && (
+          <div className="my-6 grid grid-cols-2 gap-6">
+            {product?.variants?.map((variant: ProductVariant) => (
+              <div key={variant.id}>
+                {variant.selectedOptions.map((option) => (
+                  <Button
+                    className="border border-zinc-700"
+                    size="lg"
+                    variant={"secondary"}
+                    key={option.value}
+                  >
+                    {option.value}
+                  </Button>
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
+
         <Button size={"xl"} className="mt-8 w-full">
           AJOUTER AU PANNIER
         </Button>
-
-        <div>
-          {product?.variants?.map((variant: ProductVariant) => (
-            <div key={variant.id}>
-              {variant.selectedOptions.map((option) => (
-                <Button key={option.value}>{option.value}</Button>
-              ))}
-            </div>
-          ))}
-        </div>
       </div>
     </section>
   );
