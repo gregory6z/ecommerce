72feef6e7d00b492a792049f233fb4a877b3c917
gregory6z <gregoryrag@gmail.com>
2025-02-03 22:51:18 +0100

    optimization ui large

--- src/app/(app)/(home)/components/banner-text-overlay.tsx
+++ src/app/(app)/(home)/components/banner-text-overlay.tsx
@@ -3,10 +3,13 @@ import { Button } from "@/components/ui/button";
 export function BannerTextOverlay() {
   return (
     <div className="absolute inset-0 z-200 flex flex-col justify-end rounded bg-opacity-50 p-4 text-zinc-100 md:left-16 md:justify-center md:text-zinc-900 lg:max-w-[508px]">
+      <p className="mb-8 font-semibold text-zinc-500 uppercase tracking-wide">
+        La beauté naturelle, simplement sophistiquée
+      </p>
       <h1 className="mx-auto text-pretty text-center font-bold text-2xl md:mx-0 lg:text-left lg:text-5xl">
         NOUVEAU : ÉCLAT NATUREL
       </h1>
-      <p className="mt-2 hidden md:mt-6 md:block md:text-lg">
+      <p className="hidden font-semibold text-zinc-700 md:mt-8 md:block md:text-lg">
         Découvrez la beauté naturelle de votre peau avec notre gamme de soins,
         conçue pour nourrir et revitaliser votre éclat au quotidien.
       </p>
@@ -15,9 +18,9 @@ export function BannerTextOverlay() {
       </p>
       <Button
         size={"xl"}
-        className="mt-4 bg-zinc-100 text-zinc-900 md:bg-zinc-900 md:text-zinc-100 lg:mt-6 lg:w-fit"
+        className="mt-6 bg-zinc-100 text-zinc-900 md:bg-zinc-900 md:text-zinc-100 lg:mt-10 lg:w-fit"
       >
-        DÉCOUVREZ NOS PRODUITS
+        DÉCOUVREZ VOTRE ROUTINE IDÉALE
       </Button>
     </div>
   );
--- src/components/layout/usp/index.tsx
+++ src/components/layout/usp/index.tsx
@@ -1,8 +1,8 @@
 export function Usp() {
   return (
     <div className="hidden h-12 items-center justify-center bg-zinc-100 lg:flex ">
-      <p className="font-bold text-xs uppercase">
-        livraison gratuite pour les achats de plus de 60 € 🚚
+      <p className="font-bold text-[14px] uppercase underline">
+        livraison gratuite pour les achats de plus de 60 €
       </p>
     </div>
   );
